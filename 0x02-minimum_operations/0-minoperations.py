e1ae40cbf5465f76d1c18431c73c10d1e6df8b30
Ayoub-oumha <Ayyouboumha@gmail.com>
2024-06-13 15:47:58 +0000

    commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Minimum Operations
+    """
+
+
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    next = 'H'
+    body = 'H'
+    ope = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            ope += 2
+            next = body
+            body += body
+        else:
+            ope += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return ope
